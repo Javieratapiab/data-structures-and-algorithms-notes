90d040d92570f995ca25475856bac155c23f712d
javieratapiab <javieratapiabobadilla@gmail.com>
2021-02-07 02:00:23 -0300

    feat: added suduko exercise solution.

--- python-refresher/function-and-generators.py
+++ python-refresher/function-and-generators.py
@@ -6,7 +6,9 @@ def sum_redo(a, b):
     return a + b
 
 
-# Example function 2: return the size of list, and modify the list to now be sorted.
+#
+#
+# # Example function 2: return the size of list, and modify the list to now be sorted.
 def list_sort(my_list):
     my_list.sort()
     return len(my_list), my_list
@@ -92,3 +94,89 @@ for i in range(num):
 # 6
 # 24
 # 120
+
+
+# Exercise 2
+# In the next exercise, you will write a function that checks sudoku squares for correctness.
+# Sudoku is a logic puzzle where a game is defined by a partially filled 9 x 9
+# square of digits where each square contains one of the digits 1, 2, 3, 4, 5, 6, 7, 8, 9.
+# For this question we will generalize and simplify the game.
+
+# Define a procedure, check_sudoku, that takes as input a square list of lists representing an n x n
+# sudoku puzzle solution and returns the boolean True if the input is a valid sudoku square
+# and returns the boolean False otherwise.
+
+# A valid sudoku square satisfies these two properties:
+# Each column of the square contains each of the whole numbers from 1 to n exactly once.
+# Each row of the square contains each of the whole numbers from 1 to n exactly once.
+# You may assume that the input is square and contains at least one row and column.
+
+correct = [[1, 2, 3],
+           [2, 3, 1],
+           [3, 1, 2]]
+
+incorrect = [[1, 2, 3, 4],
+             [2, 3, 1, 3],
+             [3, 1, 2, 3],
+             [4, 4, 4, 4]]
+
+incorrect2 = [[1, 2, 3, 4],
+              [2, 3, 1, 4],
+              [4, 1, 2, 3],
+              [3, 4, 1, 2]]
+
+incorrect3 = [[1, 2, 3, 4, 5],
+              [2, 3, 1, 5, 6],
+              [4, 5, 2, 1, 3],
+              [3, 4, 5, 2, 1],
+              [5, 6, 4, 3, 2]]
+
+incorrect4 = [['a', 'b', 'c'],
+              ['b', 'c', 'a'],
+              ['c', 'a', 'b']]
+
+incorrect5 = [[1, 1.5],
+              [1.5, 1]]
+
+
+# Define a function check_sudoku() here:
+def check_sudoku(square):
+    # for rows
+    for row in square:
+        # Create a list with the integers 1, 2, ..., n.
+        # We will check that each number in the row is in the list
+        # and remove the numbers from the list once they are verified
+        # to ensure that each number only occurs once in the row.
+        check_list = list(range(1, len(square[0]) + 1))
+        for num in row:
+            if num not in check_list:
+                return False
+            check_list.remove(num)
+    # fow cols
+    for col in range(len(square[0])):
+        # We do the same here for each column in the square.
+        check_list = list(range(1, len(square[0]) + 1))
+        for row in square:
+            if row[col] not in check_list:
+                return False
+            check_list.remove(row[col])
+    return True
+
+
+print(check_sudoku(incorrect))
+# >>> False
+
+print(check_sudoku(correct))
+# >>> True
+
+print(check_sudoku(incorrect2))
+# >>> False
+
+print(check_sudoku(incorrect3))
+# >>> False
+
+print(check_sudoku(incorrect4))
+# >>> False
+
+print(check_sudoku(incorrect5))
+# >>> False
